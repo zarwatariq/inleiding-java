6d7462d0298a12ec825f06303ffd5cb2d662ba59
Piet de Vries <p.h.devries@gmail.com>
2016-08-31 12:46:52 +0200

    initial commit

---
+++ src/h01/HalloRocDev.java
@@ -0,0 +1,11 @@
+package h01;
+
+/**
+ * Created by piet on 31-08-16.
+ */
+public class HalloRocDev {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo roc-dev!");
+    }
+}
