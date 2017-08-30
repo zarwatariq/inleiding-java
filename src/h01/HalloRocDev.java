0c943b5939c5a64eeec1a4e7963e3fea71014a62
Piet de Vries <p.h.devries@gmail.com>
2017-08-30 13:06:14 +0200

    Tekst veranderd

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo roc-dev!");
+        System.out.println("Hallo klas 7A!");
     }
 }
